15ef2333b34160514d67049effa36a84d1574ad5
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-08-18 16:44:08 -0500

    added react-router-dom and nav-link

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,31 @@
-import './App.css'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SingIn from '../SingIn'
+import NavBar from '../../Components/Navbar'
+import './App.css'
 
+const AppRoutes = () => {
 
-function App() {
-  return (
-    <div className="bg-red-300">
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SingIn />
-    </div>
+  let routes = useRoutes([
+    {path:'/', element:<Home />},
+    {path:'/my-account', element:<MyAccount />},
+    {path:'/my-order', element:<MyOrder />},
+    {path:'/my-orders', element:<MyOrders />},
+    {path:'/*', element:<NotFound />},
+    {path:'/sing-in', element:<SingIn />},
+  ])
+  return routes
+}
+  const App = ()=> {
+    return (
+      <BrowserRouter>
+        <AppRoutes/>
+        <NavBar/>
+      </BrowserRouter>
   )
 }
 
