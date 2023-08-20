3ea35e73df7fad173e68e506f6a9f30a7ed8c919
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-08-20 11:49:59 -0500

    added API for every card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
+  useEffect (()=> {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(responde => responde.json())
+      .then(data => setItems(data))
+  },[])
+
   return (
     <Layout>
       Home
+      <div className="grid grid-cols-4 gap-10 w-full max-w-screen-lg">
+      {
+        items?.map(item=> (
+          <Card key={item.id} data={item}/>
+        ))
+      }
+      </div>
       <Card/>
     </Layout>
   )
