c5f8de5a0cd0687ec5ecad6be0523ffce7acb6e5
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-08-27 15:34:27 -0500

    modificated API for fakestoryapi

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,22 +5,21 @@ import Card from '../../Components/Card'
 function Home() {
   const [items, setItems] = useState(null)
   useEffect (()=> {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(responde => responde.json())
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
       .then(data => setItems(data))
   },[])
 
   return (
     <Layout>
       Home
-      <div className="grid grid-cols-4 gap-10 w-full max-w-screen-lg">
+      <div className="grid gap-8 grid-cols-4 w-full max-w-screen-lg">
       {
-        items?.map(item=> (
+        items?.map(item => (
           <Card key={item.id} data={item}/>
         ))
       }
       </div>
-      <Card/>
     </Layout>
   )
 }
