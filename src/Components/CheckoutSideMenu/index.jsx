a5dc41287a26e855224d4ce8f22d0bf656cb43a1
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-09-01 11:13:48 -0500

    created counter of total  price in Ckeckour Side Menu

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -3,10 +3,17 @@ import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../Context";
 import OrderCard from "../OrderCard";
+import { totalPrice } from "../../Utils";
 import "./styles.css";
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
+
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id != id)
+    context.setCartProducts(filteredProducts)
+  }
+
   return (
     <aside className={`${context.IsCheckoutSideMenuOpen ? "flex" : "hidden"}
     checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -16,17 +23,25 @@ const CheckoutSideMenu = () => {
           <XMarkIcon className="h-6 w-6 text-black-500" />
         </div>
       </div>
-      <div className="px-6">
+      <div className="px-6 overflow-y-scroll">
       {
         context.cartProducts.map(product => (
             <OrderCard
             key = {product.id}
+            id = {product.id}
             title = {product.title}
             imageUrl = {product.image}
             price = {product.price}
+            handleDelete = {handleDelete}
             />
         ))
       }
+      </div>
+      <div className="px-6">
+        <p className="flex justify-between items-center p-4">
+            <span className="font-normal">Total:</span>
+            <span className="font-bold text-lg">${totalPrice(context.cartProducts)}</span>
+        </p>
       </div>
     </aside>
   );
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const OrderCard = props => {
-    const {id, title, imageUrl, price} = props
+    const {id, title, imageUrl, price, handleDelete} = props
 
   return (
     <div className="flex justify-between items-center mb-4">
         <div className="flex items-center gap-2">
             <figure className="w-20 h-20">
-                <img className=" w-full h-full rounded-lg object-cover" src={imageUrl} alt={title} />
+                <img className=" w-full h-full rounded-lg object-contain" src={imageUrl} alt={title} />
             </figure>
             <p className="text-xs font-medium">{title}</p>
         </div>
         <div className="flex items-center gap-2">
             <p className="text-lg font-medium">${price}</p>
-            <XMarkIcon className="h-6 w-6 text-black-500" />
+            <XMarkIcon onClick={() => handleDelete(id)} className="h-6 w-6 text-black-500 cursor-pointer" />
         </div>
     </div>
   )
