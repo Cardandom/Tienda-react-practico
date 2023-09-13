4edfd1695d304070982d0e38fad775a0fbf802ad
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-09-13 16:43:21 -0500

    styling orders card

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 const OrdersCard = props => {
     const {totalPrice, totalProducts} = props
 
   return (
-    <div className="flex justify-between items-center mb-3 border border-black">
-        <p>
-            <span>01.02.2023</span>
-            <span>{totalProducts}</span>
-            <span>{totalPrice}</span>
-        </p>
+    <div className="flex justify-between items-center mb-4 border border-black rounded-lg p-4 w-80">
+        <div className="flex justify-between w-full">
+          <p className="flex flex-col">
+            <span className="font-normal">01.02.2023</span>
+            <span>{totalProducts} Articles</span>
+          </p>
+          <p className="flex items-center gap-2">
+            <span className="font-bold text-lg">${totalPrice}</span>
+            <ChevronRightIcon className="h-6 w-6 text-black-500" />
+          </p>
+        </div>
     </div>
   )
 }
